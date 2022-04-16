e65a19432d627a35b2f7419e8762101584d47da9
mmirzay <mirzay.mohsen@gmail.com>
2022-04-16 22:23:48 +0430

    added final project main files as a simple bank management system.

---
+++ BankSimpleManagementSystem/src/main/java/ir/tosan/projects/banksimplemanagementsystem/BankSimpleManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package ir.tosan.projects.banksimplemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSimpleManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankSimpleManagementSystemApplication.class, args);
+        System.out.println("Tosan Final Project: Simple Bank Management System");
+    }
+
+}
---
+++ BankSimpleManagementSystem/src/test/java/ir/tosan/projects/banksimplemanagementsystem/BankSimpleManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.tosan.projects.banksimplemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSimpleManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
